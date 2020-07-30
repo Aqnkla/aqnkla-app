5530dd515aa472fc0c9ca9c617724211ec796442
Mariusz Nowak <mjn.dev@outlook.com>
2020-07-30 23:37:31 +0200

    aqnkla fix names

---
+++ src/.net/Aqnkla/Base/Aqnkla.Domain/ExceptionAqnkla/AqnklaNullException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Aqnkla.Domain.ExceptionAqnkla
+{
+    [Serializable]
+    public class AqnklaNullException : Exception
+    {
+        public AqnklaNullException(string message) : base(message)
+        {
+        }
+
+        public AqnklaNullException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public AqnklaNullException()
+        {
+        }
+
+
+
+        protected AqnklaNullException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
