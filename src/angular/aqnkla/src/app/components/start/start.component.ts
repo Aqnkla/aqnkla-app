886a602bf7789d2e0acdb474358a01a4893be64d
MJN <mjn.dev@outlook.com>
2020-08-15 21:35:51 +0200

    update localize

--- src/angular/aqnkla/src/app/components/start/start.component.ts
+++ src/angular/aqnkla/src/app/components/start/start.component.ts
@@ -1,18 +1,27 @@
-import { Subscription } from 'rxjs';
+import { Subscription, from } from 'rxjs';
 import { Component } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
 import { Router } from '@angular/router';
+import { NetworkService } from './../../services/network/network.service';
 
 @Component({
   selector: 'aqn-start',
-  template: '<router-outlet></router-outlet>'
+  template: '<router-outlet></router-outlet>',
 })
 export class StartComponent {
   private $messageHandler: Subscription[] = [];
   constructor(
     private router: Router,
-    private authenticationService: AuthenticationService) {
-    this.$messageHandler.push(this.authenticationService.authenticationChange.subscribe(b => this.auhticatedChange(b)));
+    private authenticationService: AuthenticationService,
+    private networkService: NetworkService
+  ) {
+    this.$messageHandler.push(
+      this.authenticationService.authenticationChange.subscribe((b) =>
+        this.auhticatedChange(b)
+      )
+    );
+    const message = networkService.get();
+    console.log('http get', message);
   }
 
   private auhticatedChange(isAuhticated: boolean): void {
